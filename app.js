b4990da6a58c3a6b83a1a6db918c27e1ffe1a43a
Afe Ayo Sunday <136712366+Afeayo@users.noreply.github.com>
2026-04-02 21:17:32 +0000

    comit doker

--- app.js
+++ app.js
@@ -43,10 +43,8 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Connected to MongoDB Atlas');
 
-        const PORT = process.env.PORT || 8000;
-        app.listen(PORT, '0.0.0.0', () => {
-            console.log(`Server running on ${PORT}`);
-        });
+        const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
 
     })
     .catch((error) => {
